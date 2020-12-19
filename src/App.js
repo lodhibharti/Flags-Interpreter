74be411679ac060d70beee3eda80b1ac4cdb38ba
Bharti Lodhi <62990284+lodhibharti@users.noreply.github.com>
2020-12-19 12:20:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "🏁": "Chequered Flag",
+  "🚩": "Triangular Flag",
+  "🎌": "Crossed Flags",
+  "🏴": "Black Flag",
+  "🏳️": "White Flag",
+  "🏳️‍🌈": "Rainbow Flag",
+  "🏳️‍⚧️": "Transgender Flag",
+  "🏴‍☠️": "Pirate Flag"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    setMeaning(meaning);
+
+    if (meaning === undefined) {
+      meaning = "Sorry! we don't have the meaning of this flag.";
+    }
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello! Welcome</h1>
+      <h2>Flags Emoji</h2>
+
+      <input onChange={emojiInputHandler} />
+
+      <h2>{meaning}</h2>
+
+      <h3>Emojis we know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            onClick={() => emojiClickHandler(emoji)}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
